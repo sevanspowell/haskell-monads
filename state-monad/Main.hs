cb3e4a709a72f8a17122f8525475cea6d818c536
Samuel Evans-Powell <samueldavid.ep@gmail.com>
2018-03-14 17:09:48 +0800

    Finish 11.9

--- state-monad/Main.hs
+++ state-monad/Main.hs
@@ -1,14 +1,22 @@
 module Main where
 
-import           Control.Monad.Reader  (Reader, ask, local, runReader)
-import           Control.Monad.State   (State, StateT, evalState, execState,
-                                        get, modify, put, runState)
-import           Control.Monad.Writer  (Writer, tell, runWriter, execWriter)
-import           Data.Foldable         (traverse_)
+import           Control.Monad.Except         (catchError, throwError)
+import           Control.Monad.Reader         (Reader, ReaderT, ask, local,
+                                               runReader, runReaderT)
+import           Control.Monad.State          (State, StateT, evalState,
+                                               execState, get, modify, put,
+                                               runState, runStateT)
+import           Control.Monad.Trans.Class    (lift)
+import           Control.Monad.Trans.Except   (ExceptT, runExceptT)
+import           Control.Monad.Trans.Identity (IdentityT, runIdentityT)
+import           Control.Monad.Writer         (Writer, WriterT, execWriter,
+                                               runWriter, runWriterT, tell)
+import           Data.Foldable                (traverse_)
 import           Data.Functor.Identity
-import           Data.List             (intercalate)
-import           Data.Monoid           (Sum(..))
-import           Data.Word             (Word)
+import           Data.List                    (drop, intercalate, stripPrefix,
+                                               take)
+import           Data.Monoid                  (Sum (..))
+import           Data.Word                    (Word)
 
 test :: State Int Int
 test = do
@@ -19,7 +27,7 @@ test = do
 sumArray :: [Int] -> State Int ()
 sumArray = traverse_ (\n -> modify (\sum' -> sum' + n))
 
-sumArray' :: [Int] -> Writer (Sum Int) () 
+sumArray' :: [Int] -> Writer (Sum Int) ()
 sumArray' = traverse_ (\n -> tell (Sum n))
 
 testSumArray :: IO ()
@@ -119,7 +127,7 @@ gcdLog n m = do
     else gcdLog n (m - n)
 
 -- Number of iterations for collatz sequence to reach 1.
-collatzNum :: Word -> Word 
+collatzNum :: Word -> Word
 collatzNum x = collatz x
   where
     collatz :: Word -> Word
@@ -130,7 +138,7 @@ collatzNum x = collatz x
         False -> do
           1 + collatz (3 * n + 1)
 
-collatzNum' :: Word -> Writer [String] Word 
+collatzNum' :: Word -> Writer [String] Word
 collatzNum' x = collatz x
   where
     collatz :: Word -> Writer [String] Word
@@ -146,6 +154,97 @@ collatzNum' x = collatz x
           tell [show n]
           cnt <- collatz (3 * n + 1)
           pure $ cnt + 1
+
+split :: StateT String (Either String) String
+split = do
+  s <- get
+  case s of
+    "" -> lift $ Left "Empty string"
+    _  -> do
+      put (drop 1 s)
+      pure (take 1 s)
+
+writerAndExceptT :: ExceptT String (Writer [String]) String
+writerAndExceptT = do
+  lift $ tell ["Before the error"]
+  throwError "Error!"
+  lift $ tell ["After the error"]
+  pure "Return value"
+
+data Errors = EmptyString
+  deriving Show
+
+type Log = [String]
+
+type Parser = StateT String (ExceptT Errors (WriterT Log Identity))
+
+split' :: Parser String
+split' = do
+  s <- get
+  lift $ lift $ tell ["The state is " ++ show s]
+  case s of
+    "" -> lift $ throwError EmptyString
+    _  -> do
+      put (drop 1 s)
+      pure (take 1 s)
+
+runParser :: Parser a -> String -> (Either Errors (a, String), Log)
+runParser p s = runIdentity $ runWriterT $ runExceptT $ runStateT p s
+
+safeDivide :: (Fractional a, Eq a) => a -> a -> ExceptT String Identity a
+safeDivide _ 0 = throwError "Denominator is zero"
+safeDivide a b = pure (a / b)
+
+type Parser' = StateT String (WriterT Log (ExceptT String Identity))
+
+runParser' :: Parser' a -> String -> Either String ((a, String), Log)
+runParser' p s = runIdentity $ runExceptT $ runWriterT $ runStateT p s
+
+string :: String -> Parser' String
+string prefix = do
+  str <- get
+  lift $ tell ["The state is " ++ show str]
+  let maybeNoPrefix = stripPrefix prefix str
+  case maybeNoPrefix of
+    Nothing         -> lift $ lift $ throwError ("Prefix '" ++ prefix ++ "' not present.")
+    (Just noPrefix) -> do
+      put noPrefix
+      pure prefix
+
+type Level' = Int
+
+type Doc' = ReaderT Level (WriterT [String] Identity) ()
+
+renderDoc' :: Doc' -> String
+renderDoc' doc = cat' (snd . runIdentity $ runWriterT $ runReaderT doc 0)
+  where
+    cat' :: [String] -> String
+    cat' = intercalate "\n"
+
+line' :: String -> Doc'
+line' xs = do
+  level <- ask
+  let lne = render level xs
+  lift $ tell [lne]
+  where
+    render :: Level -> String -> String
+    render 0 str = str
+    render l str = render (l - 1) (" " ++ str)
+
+indent' :: Doc' -> Doc'
+indent' = local (+ 1)
+
+testDoc :: Doc'
+testDoc = do
+  line' "Here is some indented text: "
+  indent' $ do
+    line' "I am indented"
+    line' "So am I"
+    indent' $ do
+      line' "I am even more indented"
+
+testReader' :: String
+testReader' = renderDoc' testDoc
 
 main :: IO ()
 main = print $ execState test 0
