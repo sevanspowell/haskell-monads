999b4e381ff7e1b5324f5db916fcc20ac4a7ad27
Samuel Evans-Powell <samueldavid.ep@gmail.com>
2018-03-26 23:42:33 +1100

    Basic audio playing

--- game/Main.hs
+++ game/Main.hs
@@ -1,10 +1,12 @@
 module Main where
 
+import Control.Monad
 import Control.Monad.RWS.Lazy (runRWS, liftIO)
 import Data.List.Split (splitOn)
 import Data.Foldable (for_)
 
 import System.Console.Haskeline
+import Sound.ALUT
 
 import Data.GameEnvironment
 import Data.GameState
@@ -30,5 +32,25 @@ runGame env state = runInputT defaultSettings go
                 for_ written putStrLn
                 runGame env newState
 
+-- Chris Double
+-- https://bluishcoder.co.nz/articles/haskell/openal.html
+playSound :: IO ()
+playSound =
+  withProgNameAndArgs runALUTUsingCurrentContext $ \_ _ ->
+  do
+    (Just device) <- openDevice Nothing
+    (Just context) <- createContext device []
+    currentContext $= Just context
+    buffer1 <- createBuffer $ Sine 440 0 1
+    buffer2 <- createBuffer HelloWorld
+    [source] <- genObjectNames 1
+    queueBuffers source [buffer1,buffer2]
+    play [source]
+    sleep 4
+    closeDevice device
+    return ()
+
 main :: IO ()
 main = runGame defaultEnv initialGameState
+-- main = playSound
+
